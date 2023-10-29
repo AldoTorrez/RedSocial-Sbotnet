536f09b9a8b8aa7ffb56f91e9d028ef23ed27d96
AldoTorrez <adlotorrez91@gmail.com>
2023-10-28 21:21:46 -0400

    mysql conection with nestjs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UsersModule],
+  imports: [SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'mysql',
+    password: '/Barcoder123',
+    database: 'Sbotnet',
+    models: []
+  }),
+    UsersModule],
   controllers: [],
   providers: [],
 })
