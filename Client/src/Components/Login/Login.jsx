fe7988af9e3a7fbfa182656a0af25ad28b43cb9c
AldoTorrez <adlotorrez91@gmail.com>
2023-10-30 21:36:09 -0400

    first commit

--- Client/src/Components/Login/Login.jsx
+++ Client/src/Components/Login/Login.jsx
@@ -2,13 +2,6 @@ export default function Login(){
     return(
         <div>
             Este es el login XD
-            <h1>Este es el compoente de Login que lo vamos a modificar por que me parece que se vera mejor si solo
-                ponemos el login en medio de la pagina seria mas sensillo y entendible no se necesita mas
-                y este proyecto se debe acabar los mas rapido posible lo iimportante de este proyecto en si es aprender un monton 
-                luego de esto armeos mas proyecto que aporten valor a mi portafolio e lo que vendria sienod mas tecnolgias
-                lo impotante es poder tener mi github activo para que los reclutadores vean que estoy programando todo
-                el tiempo y que estoy en constante aprendisaje
-            </h1>
         </div>
     )
 }
