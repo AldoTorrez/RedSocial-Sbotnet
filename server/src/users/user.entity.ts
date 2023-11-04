0f265c3e42b3ac9da41ccae8f897ac689785537d
AldoTorrez <adlotorrez91@gmail.com>
2023-11-03 20:55:44 -0400

    add more columns to the users table

--- server/src/users/user.entity.ts
+++ server/src/users/user.entity.ts
@@ -1,22 +1,42 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
+enum Gender {
+    HOMBRE = 'hombre',
+    MUJER = 'mujer',
+    NO_BINARIO = 'no binario',
+    OTRO = 'otro',
+    VACIO = ''
+}
+
 @Entity('users')
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+    @PrimaryGeneratedColumn("uuid")
+    id: string
 
     @Column()
     username: string
 
     @Column()
+    name: string
+
+    @Column({unique: true})
     email: string
 
     @Column()
     password: string
 
-    @Column()
-    profilePicture: string
+    @Column({type: 'datetime'})
+    registrationDate: string
+
+    @Column({type: 'datetime'})
+    lastSession: string
+
+    @Column({type: 'date'})
+    dateOfBirth: string
+
+    @Column({type: 'enum', enum:Gender, default: Gender.VACIO})
+    gender: Gender;
 
     @Column()
-    name: string
+    profilePicture: string
 }
